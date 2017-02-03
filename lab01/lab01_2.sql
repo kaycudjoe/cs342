4f87382b33b490b60afb0fae53c2f25c3f6cbaf8
kaycudjoe <kec32@students.calvin.edu>
2017-02-03 14:25:16 -0500

    Add files via upload

---
+++ lab01/lab01_2.sql
@@ -0,0 +1,11 @@
+-- Karen Cudjoe
+-- CS 342, lab01_1
+
+SELECT * FROM dual;
+
+DESCRIBE dual; 
+
+
+-- The Dual is a DUMMY table has one-row and one-column. It has a type of VARCHAR(2).
+-- It has one-row which has a value of 'X' 
+-- One might want to use the DUAL Table when one is interested in selecting a pseudo column like USER
