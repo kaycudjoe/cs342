bd79cffbbe6454750c8ef0a5d5bd848425cea342
kaycudjoe <kec32@students.calvin.edu>
2017-03-03 19:56:44 -0500

    Add files via upload

---
+++ project/drop.sql
@@ -0,0 +1,10 @@
+--drop tables in Calvin LifeWork database
+drop table GraduateEmployer;
+drop table StudentEmployer;
+drop table StudentRequirement;
+drop table Requirement;
+drop table YearRequired;
+drop table Employer;
+drop table Student;
+drop table Staff;
+drop table Graduate;
