f0151949ea1175943bcbd7f538c060c0234095fe
kaycudjoe <kec32@students.calvin.edu>
2017-05-11 17:30:21 -0400

    Updated dump files

--- project/create.sql
+++ project/create.sql
@@ -1,3 +1,6 @@
+-- Create the Calvin Lifework user and database. 
+-- See ../README.txt for details.
+
 -- Create the user.
 DROP USER calvinLW CASCADE;
 CREATE USER calvinLW
@@ -15,6 +18,18 @@ GRANT
 	UNLIMITED TABLESPACE
 	TO calvinLW;
 
+-- Connect to the user's account/schema.
+CONNECT calvinLW/kec32password;
+
+
+-- Set up the Oracle directory for the dump database feature.
+-- Use Oracle directories for input/output files to avoid permissions problems. (?)
+-- This is needed both to create and to load the *.dmp files.
 DROP DIRECTORY exp_dir;
 CREATE DIRECTORY exp_dir AS 'C:\Users\kec32\Documents\project';
 GRANT READ, WRITE ON DIRECTORY exp_dir to calvinLW;
+
+
+-- Create the database.
+DEFINE calvinLW=C:\Users\kec32\Documents\project
+@&calvinLW\load
