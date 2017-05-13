94e6b766b1c6ffd259cf0d55076611ea0abdb96c
kaycudjoe <kec32@students.calvin.edu>
2017-05-13 09:56:01 -0400

    nosql-incomplete

---
+++ project/nosql/src/GetTableValues.java
@@ -0,0 +1,34 @@
+import oracle.kv.*;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+
+
+/**
+ * Created by Karen Cudjoe
+ */
+public class GetTableValues {
+    public static void main(String[] args) throws SQLException {
+        KVStore store = KVStoreFactory.getStore(new KVStoreConfig("kvstore", "localhost:5000"));
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter table name: ");
+        String gettable = reader.nextLine();
+
+        System.out.print("Enter student ID: ");
+        Integer id = reader.nextInt();
+
+        System.out.println("\nTable: student");
+        System.out.println("ID: " + id.toString());
+
+        Key majorKeyPathOnly = Key.createKey(Arrays.asList(gettable, id.toString()));
+        Map<Key, ValueVersion> fields = store.multiGet(majorKeyPathOnly, null, null);
+        for (Map.Entry<Key, ValueVersion> field : fields.entrySet()) {
+            String fieldValue = new String(field.getValue().getValue().getValue());
+            System.out.println(fieldValue);
+        }
+        store.close();
+    }
+}
