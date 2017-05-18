81ee48733149a1cf227baf7575fa04d2ca83a2f4
kaycudjoe <kec32@students.calvin.edu>
2017-05-17 21:41:53 -0400

    Documented triggers.sql

--- project/triggers.sql
+++ project/triggers.sql
@@ -1,3 +1,9 @@
+-- gradtaken trigger checks to make sure that a graduate is not assigned too many students
+-- Each graduate can have a maximum of 3 students assigned
+-- If the insert command tries to assign more students to graduates such that a graduate will have more than 3 assigned students, an application error is raised notifying the user of the error. 
+
+
+
 CREATE OR REPLACE TRIGGER gradtaken BEFORE INSERT ON Graduate FOR EACH ROW
 DECLARE
 	COUNTER INTEGER;
@@ -5,10 +11,12 @@ DECLARE
 BEGIN
 	SELECT COUNT(*) INTO COUNTER FROM Graduate g, Student s
 	WHERE s.graduateID = g.ID;
+	-- If the graduate is already fully taken (has the maximum number of students assigned) raise an exception. 
 	IF COUNTER > 3 THEN 
 		RAISE maxnumberofStudents;
 	END IF;
 EXCEPTION
+-- if the graduate is already full
 	WHEN maxnumberofStudents THEN
 		RAISE_APPLICATION_ERROR(-20001, 'Graduate has too many students assigned.');
 END;
